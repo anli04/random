ebacd0667afe0990ec2c3fe182ec10380b640b8e
Anthony Li <ali04@stuy.edu>
2019-11-09 21:37:41 -0500

    updates

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-int * randInt();
+int randInt();
 
 int main(){
   int fd = open("nums.txt", O_RDWR | O_CREAT, 644);
@@ -14,21 +14,22 @@ int main(){
   int nums[11];
   printf("Generating random numbers:\n");
   int i = 0;
-  for (; i < 10; i++) nums[i] = *(randInt());
+  for (; i < 10; i++) nums[i] = randInt();
   if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
   i = 0;
   for (; i < 10; i++) printf("random %d: %d\n", i, nums[i]);
   printf("Writing numbers to file.\n");
-  write(fd, nums, 10 * sizeof(int));
+  write(fd, nums, sizeof(nums));
   if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
   printf("Verification that numbers are the same (by reading):\n");
-  read(fd, nums, 10 * sizeof(int));
+  read(fd, nums, sizeof(nums));
   i = 0;
   for (; i < 10; i++) printf("random %d: %d\n", i, nums[i]);
+  close(fd);
   return 0;
 }
 
-int * randInt(){
+int randInt(){
   int fd = open("/dev/random", O_RDONLY);
   if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
   int * buff;
@@ -36,5 +37,5 @@ int * randInt(){
   if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
   close(fd);
   if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
-  return buff;
+  return *buff;
 }
